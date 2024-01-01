b03458010b3e144987f9f836f096b5f6dc00e3c3
shanukumar007 <shanumehta007@gmail.com>
2024-01-01 21:45:42 +0530

    config & artifact file update

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 from Insurance.utils import get_collection_as_dataframe
 import sys, os
+from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity import config_entity
+
 
 
 
@@ -17,9 +20,14 @@ import sys, os
        # raise InsuranceException(e, sys)
 
 if __name__=="__main__":
-    try:
+     try:
           #start_training_pipeline()
           #test_logger_and_expection()
-        get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
-    except Exception as e:
+       # get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+     except Exception as e:
           print(e)   
+       
+         
