8b1eb09cccb33710a2fbb40bc11c56ecb5289203
shanukumar007 <shanumehta007@gmail.com>
2024-01-01 21:08:44 +0530

    logger & exception file updated

---
+++ main.py
@@ -0,0 +1,22 @@
+from Insurance.logger import logging
+from Insurance.exception import InsuranceException
+import sys, os
+
+
+
+
+def test_logger_and_expection():
+    try:
+        logging.info("Starting the test_logger_and_exception")
+        result = 3 / 10
+        print(result)
+        logging.info("Stoping the test_logger_and_exception")
+    except Exception as e:
+        logging.debug(str(e))
+        raise InsuranceException(e, sys)
+    
+if __name__=="__main__":
+    try:
+         test_logger_and_expection()
+    except Exception as e:
+        print(e)
