7eacc1384af0c3022768f3e2c7c48c05f83f7fc9
shanukumar007 <shanumehta007@gmail.com>
2024-01-01 22:34:49 +0530

    data transformation added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
+from Insurance.components.data_transformation import DataTransformation
 
 
 
@@ -40,6 +41,12 @@ if __name__=="__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
         
        data_validation_artifact = data_validation.initiate_data_validation()
+
+       # data transformation
+       data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+       data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+       data_ingestion_artifact=data_ingestion_artifact)
+       data_transformation_artifact = data_transformation.initiate_data_transformation()
 
     except Exception as e:
           print(e)   
