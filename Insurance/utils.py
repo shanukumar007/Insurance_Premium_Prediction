f45bfd07e877a5e28af70e5b03cc25e198f660d8
shanukumar007 <shanumehta007@gmail.com>
2024-01-01 22:20:34 +0530

    data validation added

--- Insurance/utils.py
+++ Insurance/utils.py
@@ -26,7 +26,25 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
             df = df.drop("_id",axis=1)
         logging.info(f"Row and columns in df: {df.shape}")
         return df
-    
     except Exception as e:
         raise InsuranceException(e, sys)
 
+#***********************************## Data_Validation*******************************************
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise InsuranceException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                if df[column].dtypes != 'O':
+                    df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys, os
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
+from Insurance.components.data_validation import DataValidation
+
 
 
 
@@ -20,16 +22,24 @@ from Insurance.components.data_ingestion import DataIngestion
        # raise InsuranceException(e, sys)
 
 if __name__=="__main__":
-     try:
+    try:
           #start_training_pipeline()
           #test_logger_and_expection()
        # get_collection_as_dataframe(database_name ="INSURANCE", collection_name = 'INSURANCE_PROJECT')
-        training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
-        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-     except Exception as e:
-          print(e)   
+       training_pipeline_config = config_entity.TrainingPipelineConfig()
        
-         
+      #data ingestion
+       data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+       print(data_ingestion_config.to_dict())
+       data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+       data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+       
+       #data validation
+       data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+       data_validation = DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
+        
+       data_validation_artifact = data_validation.initiate_data_validation()
+
+    except Exception as e:
+          print(e)   
